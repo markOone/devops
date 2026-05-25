13a4be7d33f66199312d0b2acd025601996a418a
markOone <98772113+markOone@users.noreply.github.com>
2026-05-25 16:43:56 +0300

    fix: deployment script

--- deploy.sh
+++ deploy.sh
@@ -8,18 +8,18 @@ fi
 echo "Починаємо розгортання..."
 
 apt-get update
-apt-get install -y postgresql postgresql-contrib python3 python3-venv python3-pip nginx git curl
+apt-get install -y postgresql postgresql-contrib python3 python3-dev python3-venv python3-pip nginx git curl libpq-dev build-essential
 
-useradd -m -s /bin/bash -G sudo student
+useradd -m -s /bin/bash -G sudo student 2>/dev/null || echo "Користувач student вже існує"
 echo "student:studentpass" | chpasswd
 
-useradd -m -s /bin/bash -G sudo teacher
+useradd -m -s /bin/bash -G sudo teacher 2>/dev/null || echo "Користувач teacher вже існує"
 echo "teacher:12345678" | chpasswd
 chage -d 0 teacher
 
-useradd -r -s /usr/sbin/nologin app
+useradd -r -s /usr/sbin/nologin app 2>/dev/null || echo "Користувач app вже існує"
 
-useradd -m -s /bin/bash operator
+useradd -m -s /bin/bash operator 2>/dev/null || echo "Користувач operator вже існує"
 echo "operator:12345678" | chpasswd
 chage -d 0 operator
 
@@ -27,8 +27,8 @@ cat <<EOT > /etc/sudoers.d/operator
 operator ALL=(ALL) NOPASSWD: /usr/bin/systemctl start mywebapp.service, /usr/bin/systemctl stop mywebapp.service, /usr/bin/systemctl restart mywebapp.service, /usr/bin/systemctl status mywebapp.service, /usr/bin/systemctl reload nginx
 EOT
 
-sudo -u postgres psql -c "CREATE DATABASE taskdb;"
-sudo -u postgres psql -c "CREATE USER appuser WITH ENCRYPTED PASSWORD 'apppass';"
+sudo -u postgres psql -c "CREATE DATABASE taskdb;" 2>/dev/null || echo "БД вже існує"
+sudo -u postgres psql -c "CREATE USER appuser WITH ENCRYPTED PASSWORD 'apppass';" 2>/dev/null || echo "Користувач БД вже існує"
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE taskdb TO appuser;"
 sudo -u postgres psql -d taskdb -c "GRANT ALL ON SCHEMA public TO appuser;"
 
@@ -44,6 +44,7 @@ EOF
 chown -R app:app /etc/mywebapp
 
 APP_DIR="/opt/mywebapp"
+rm -rf $APP_DIR  
 git clone https://github.com/markOone/devops $APP_DIR
 
 python3 -m venv $APP_DIR/venv
@@ -82,6 +83,7 @@ systemctl daemon-reload
 systemctl enable mywebapp.socket
 systemctl start mywebapp.socket
 systemctl enable mywebapp.service
+systemctl restart mywebapp.service
 
 rm -f /etc/nginx/sites-enabled/default
 cat <<EOF > /etc/nginx/sites-available/mywebapp
@@ -104,7 +106,7 @@ server {
     }
 }
 EOF
-ln -s /etc/nginx/sites-available/mywebapp /etc/nginx/sites-enabled/
+ln -s /etc/nginx/sites-available/mywebapp /etc/nginx/sites-enabled/ 2>/dev/null
 systemctl restart nginx
 
 echo "7" > /home/student/gradebook
